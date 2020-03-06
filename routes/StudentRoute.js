008293cc83ee1b606eea8f5fd5a2090e3ae81414
ivettifkovics-trax <ivett.ifkovics@traxretail.com>
2020-03-06 13:58:43 +0100

    [MDC-4444 Impr] Initial commit

---
+++ routes/StudentRoute.js
@@ -0,0 +1,112 @@
+import express from 'express';
+import students from '../data/students.json';
+import _ from 'lodash';
+import mongoose from 'mongoose';
+
+const DB_URL = `mongodb://localhost:27017/training`;
+
+const router = express.Router();
+
+let studentsArray = students;
+
+mongoose.connect(DB_URL);
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('wohho we connected to the db');
+}); 
+
+const StudentSchema = mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    course: String,
+});
+
+const StudentModel = mongoose.model('Student', StudentSchema);
+
+//server.get(`${STUDENTS_BASE_URL}/:id`, (req, res) => {
+//   const student = _.find(students, student => student.id === req.params.id);
+//    if (student) {
+//        res.json(student);
+//    } else {
+//        res.send(`User with id ${req.params.id} not found.`);
+//    }
+//});
+
+router.get('/', (req, res) => {
+//    res.json(studentsArray);
+    StudentModel.find((err, students) => {
+        if (err) res.status(500).send(err);
+        res.json(students);
+
+    })
+});
+
+router.get('/:id', (req, res) => {
+    /* const paramId = parseInt(req.params.id, 10);
+    const student = _.find(studentsArray, student => student.id === paramId);
+    if (student) {
+        res.json(student);
+    } else {
+        res.send(`User with id ${req.params.id} not found.`);
+    } */
+    StudentModel.findById(req.params.id, (err, student) =>{
+        if (err) return res.status(500).send(err);
+        if (student) {
+            res.json(student);
+        } else {
+            res.status(404).send(`User with id ${req.params.id} not found.`);
+        }        
+    });
+});
+
+router.post('/', (req, res) => {
+    /*console.log("handling POST request...");
+    console.log(req.body);
+    studentsArray.push(req.body);
+    res.status(200).send("OK"); */
+    const id = new mongoose.Types.ObjectId();
+    const studentToPersist = Object.assign({
+        _id: id
+    }, req.body);
+
+    const student = new StudentModel(studentToPersist);
+
+    student.save().then((err, student) => {
+        if(err) res.status(500).send(err);
+        res.json(student);
+    })
+});
+
+router.put('/:id', (req, res) => {
+    StudentModel.findById(req.params.id, (err, student) =>{
+        if(err) res.status(500).send(err);
+        if (student) {
+        student.name = req.body.name;
+        student.course = req.body.course;
+        student.save().then((err, student) => {
+            if(err) res.status(500).send(err);
+            res.json(student);
+        });
+        } else {
+            res.status(404).send(`User with id ${req.params.id} not found.`);
+        }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    StudentModel.findByIdAndRemove(req.params.id, (err, student) => {
+        if(err) res.status(500).send(err);
+        res.status(200).send(`Student with id ${req.params.id} was deleted`); 
+    });
+});
+
+/* router.param('id', (req, res, next, id) => {
+    if (isNaN(id)) {
+        next(`${id} is not a valid number`);
+    }
+    next();
+}) */
+
+module.exports = router;
